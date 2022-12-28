283a270f991a23b9554b19f765d01e1690c9a1c2
Jerry Zhou <46613983+jerryzhou196@users.noreply.github.com>
2022-12-27 18:55:24 -0700

    :sparkles: day6

---
+++ day6/contains_duplicates.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+	int romanToInt(string s) {
+		unordered_map<char, int> vals{
+		    {'I', 1},
+		    {'V', 5},
+		    {'X', 10},
+		    {'L', 50},
+		    {'C', 100},
+		    {'D', 500},
+		    {'M', 1000},
+		};
+
+		int sum = 0;
+		int n   = s.size();
+
+		for (int x = 0; x < n; ++x) {
+			int prev = (x == 0) ? 0 : x - 1;
+			if (vals[s[prev]] < vals[s[x]]) {
+				sum -= vals[s[prev]];
+				sum += vals[s[x]] - vals[s[prev]];
+			} else {
+				sum += vals[s[x]];
+			}
+		}
+		return sum;
+	}
+};
