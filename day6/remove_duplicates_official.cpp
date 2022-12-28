5cd89d6d0a658c2ee91219c12437ef784bc8c3b1
Jerry Zhou <46613983+jerryzhou196@users.noreply.github.com>
2022-12-27 20:14:03 -0700

    :rocket: format

--- day6/remove_duplicates_official.cpp
+++ day6/remove_duplicates_official.cpp
@@ -6,7 +6,8 @@ class Solution {
 public:
 	int removeDuplicates(vector<int>& nums) {
 		int insertIndex = 1;
-		for (int i = 1; i < nums.size(); i++) {  // THIS IS ACTUALLY REALLY BAD PRACTICE AS nums.size() is O(n^2)
+		for (int i = 1; i < nums.size();
+		     i++) {  // THIS IS ACTUALLY REALLY BAD PRACTICE IN C as nums.size() is O(n^2), BUT IN C++ .size is a GETTER
 			// We skip to next index if we see a duplicate element
 			if (nums[i - 1] != nums[i]) {
 				// Storing the unique element at insertIndex index and incrementing the insertIndex by 1
