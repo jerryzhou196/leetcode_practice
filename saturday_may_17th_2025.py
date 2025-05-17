616b0462d881057b85aa0a940dc10236c680f0ef
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-05-17 19:23:00 -0400

    added mailmap

--- saturday_may_17th_2025.py
+++ saturday_may_17th_2025.py
@@ -10,8 +10,34 @@ def segregate0and1(arr):
             lo += 1
             hi -= 1
 
-arr = [0, 1, 0, 1, 1, 1]
-segregate0and1(arr)
+def sort012(arr):
+    n = len(arr)
+    lo = 0
+    hi = n - 1
+    mid = 0
+
+    # Iterate till all the elements are sorted
+    while mid <= hi:
+      if arr[mid] == 0:
+        arr[lo], arr[mid] = arr[mid], arr[lo]
+        lo = lo + 1
+        mid = mid + 1
+        
+      elif arr[mid] == 1:
+        mid = mid + 1
+        
+      else:
+        arr[mid], arr[hi] = arr[hi], arr[mid]
+        hi = hi - 1
+        
+    return arr
+
+
+
+arr = [0, 1, 2, 0, 1, 2] 
+for x in arr:
+    print(x, end=" ")
+sort012(arr)
 print("Array after segregation:", arr)
 
 if __name__ == "__main__":
