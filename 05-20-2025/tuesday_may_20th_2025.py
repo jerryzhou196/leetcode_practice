e25070187e40cd79d3cbd41056855b855e9a4651
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-05-20 09:06:39 -0400

    nice

---
+++ 05-20-2025/tuesday_may_20th_2025.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        
+
+
+
+
+# Input: nums = [1,0,1], queries = [[0,2]]
+# ans: [0,0,0]
+
+
+# Input: nums = [4,3,2,1], queries = [[1,3],[0,2]]
+# ans: [0,0,0]
+
+# Input: nums = [9, 2, 3, 4, 4, 5], queries = [[], [], []...]
+
+# max_subtract: [0, 0, 0]
+
+
