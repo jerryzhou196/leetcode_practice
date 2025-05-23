3bfe9b83dc67d2ae9b34981e52a56bdb58f62662
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-05-23 06:03:16 -0400

    nice

--- 05-22-2025/thursday_may_22nd_2025.py
+++ 05-22-2025/thursday_may_22nd_2025.py
@@ -1,7 +1,22 @@
-\"\"\"Auto-generated on Thursday, May 22, 2025.\"\"\"
+from typing import *
 
-def main() -> None:
-    print("Hello from thursday_may_22nd_2025.py")
 
-if __name__ == "__main__":
-    main()
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        deltaArray = [0] * (len(nums) + 1)
+        for left, right in queries:
+            deltaArray[left] += 1
+            deltaArray[right + 1] -= 1
+        operationCounts = []
+        currentOperations = 0
+        for delta in deltaArray:
+            currentOperations += delta
+            operationCounts.append(currentOperations)
+        for operations, target in zip(operationCounts, nums):
+            if operations < target:
+                return False
+        return True
+
+
+s = Solution()
+s.isZeroArray([4, 3, 2, 1], queries=[[1, 3], [0, 2]])
