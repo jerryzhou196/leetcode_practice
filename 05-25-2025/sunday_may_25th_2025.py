ad1a54097c913d522eae3686450cce6849832e73
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-05-26 08:40:33 -0400

    nice

--- 05-25-2025/sunday_may_25th_2025.py
+++ 05-25-2025/sunday_may_25th_2025.py
@@ -1,7 +1,25 @@
-\"\"\"Auto-generated on Sunday, May 25, 2025.\"\"\"
+from typing import *
 
-def main() -> None:
-    print("Hello from sunday_may_25th_2025.py")
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        array = []
+        dp = [0 for _ in range(len(nums))]
 
-if __name__ == "__main__":
-    main()
+        def dfs(index, parity = True): 
+            if index == len(nums):
+                if parity: return 0
+                if not parity: return -float('inf')
+        
+            return max(dfs(index + 1, not parity) + (nums[index] ^ k),
+                     dfs(index + 1, parity) + (nums[index]))
+        
+        return dfs(0)
+
+nums = [7,7,7,7,7,7]
+k = 3
+edges = [[0,1],[0,2],[0,3],[0,4],[0,5]]
+s = Solution()
+
+print(s.maximumValueSum(nums, k, edges))
+
+
---
+++ 05-26-2025/ leetcode_hard.py
@@ -0,0 +1,57 @@
+from typing import *
+from collections import defaultdict
+
+class Solution:
+    def isPalindrome(self, str): 
+        m = len(str) // 2
+        for i in range(m):
+            if str[i] != str[len(str) - i - 1]: 
+                return False
+        
+        return True
+
+    def reverseString(self,str): 
+        ans = []
+        for char in range(len(str) - 1,  -1, -1): 
+            ans.append(str[char])
+        return "".join(ans)
+
+    def longestPalindrome(self, words: List[str]) -> int:
+        want = defaultdict(int)
+        ans = 0
+
+        for word in words: 
+            if word in want:
+                ans += len(word) * 2
+                if want[word] == 1: 
+                    del want[word]
+                else: 
+                    want[word] -= 1
+            else: 
+                want[self.reverseString(word)] += 1 
+
+        largest_single_palindrome = 0
+
+        for key in want.keys(): 
+            if self.isPalindrome(key): 
+                largest_single_palindrome = max(largest_single_palindrome, len(key))
+
+        ans += largest_single_palindrome 
+        
+        return ans
+
+s = Solution()
+print(s.longestPalindrome(["qo","fo","fq","qf","fo","ff","qq","qf","of","of","oo","of","of","qf","qf","of"]))
+
+# print(sorted(["qo","fo","fq","qf","fo","ff","qq","qf","of","of","oo","of","of","qf","qf","of"]))
+# print(s.isPalindrome("eeracecaree"))
+
+# fo
+
+['ff', 'fo', 'fo', 'fq', 'of', 'of', 'of', 'of', 'of', 'oo', 'qf', 'qf', 'qf', 'qf', 'qo', 'qq']
+
+
+
+
+        
+        
