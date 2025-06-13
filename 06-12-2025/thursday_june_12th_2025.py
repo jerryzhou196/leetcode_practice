a1b0f40584cd9451e9f460db38d7b5cca976c1e9
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-06-13 08:38:49 -0400

    🥳 Kicking off friday_june_13th_2025.py on 2025-06-13

--- 06-12-2025/thursday_june_12th_2025.py
+++ 06-12-2025/thursday_june_12th_2025.py
@@ -1,7 +1,39 @@
-"""Auto-generated on Thursday, June 12, 2025."""
+import heapq
 
-def main() -> None:
-    print("Hello from thursday_june_12th_2025.py")
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        nice = [] 
+        ans = ""
 
-if __name__ == "__main__":
-    main()
+        rev = s[::-1]
+        
+        for index, char in enumerate(rev):
+            heapq.heappush(nice, (char, index))
+
+        n = len(rev)
+        min_index = n
+
+
+        
+        while nice:
+            char, idx = heapq.heappop(nice)
+            if idx < min_index:
+                ans += rev[idx:min_index]
+                min_index = min(min_index, idx)
+        
+        return ans
+            
+
+s = Solution()
+print(s.robotWithString("bac"))
+
+
+
+
+
+
+
+
+
+    
+        
---
+++ 06-13-2025/friday_june_13th_2025.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Friday, June 13, 2025."""
+
+def main() -> None:
+    print("Hello from friday_june_13th_2025.py")
+
+if __name__ == "__main__":
+    main()
