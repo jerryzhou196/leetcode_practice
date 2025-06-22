80d387d45abe8249752ab870a5c34384f98b2b2e
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-06-21 20:12:43 -0400

    finished

--- 06-10-2025/tuesday_june_10th_2025.py
+++ 06-10-2025/tuesday_june_10th_2025.py
@@ -1,5 +1,23 @@
-def main() -> None:
-    print("Hello from tuesday_june_10th_2025.py")
+class Solution:
+    def lastSubstring(self, s: str) -> str:
+        i, j, n = 0, 1, len(s)
+        while j < n:
+            k = 0
+            while j + k < n and s[i + k] == s[j + k]:
+                k += 1
+            if j + k < n and s[i + k] < s[j + k]:
+                i, j = j, max(j + 1, i + k + 1)
+            else:
+                j = j + k + 1
+        return s[i:]
 
-if __name__ == "__main__":
-    main()
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        last = self.lastSubstring(word)
+        n, m = len(word), len(last)
+        return last[: min(m, n - numFriends + 1)]
+
+
+s = Solution()
+print(s.answerString("axyzzazzzmalkf", 2))
