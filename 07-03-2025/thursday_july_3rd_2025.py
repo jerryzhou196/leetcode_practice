00221cb859df915d6271e51f87b1996152775240
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-07-04 09:01:14 -0400

    🚀 Initial commit for friday_july_4th_2025.py on 2025-07-04

--- 07-03-2025/thursday_july_3rd_2025.py
+++ 07-03-2025/thursday_july_3rd_2025.py
@@ -1,7 +1,17 @@
-"""Auto-generated on Thursday, July 03, 2025."""
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        x, y = len(matrix), len(matrix[0])
+        def getVal(val):
+            row = val // y
+            col = val % y
+            return matrix[row][col]
 
-def main() -> None:
-    print("Hello from thursday_july_3rd_2025.py")
-
-if __name__ == "__main__":
-    main()
+        left, right = 0, (x * y) - 1
+        while left <= right:
+            m = (left + right) // 2
+            m_val = getVal(m)
+            if m_val == target: return True
+            if target < m_val: right = m - 1
+            elif target > m_val: left = m + 1
+        
+        return False
---
+++ 07-04-2025/friday_july_4th_2025.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Friday, July 04, 2025."""
+
+def main() -> None:
+    print("Hello from friday_july_4th_2025.py")
+
+if __name__ == "__main__":
+    main()
