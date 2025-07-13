5ddcd75869ea03c00a8e78df2fcff0de06284194
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-07-13 13:44:57 -0400

    🎉 Began work on sunday_july_13th_2025.py on 2025-07-13

--- 07-12-2025/saturday_july_12th_2025.py
+++ 07-12-2025/saturday_july_12th_2025.py
@@ -1,7 +1,16 @@
-"""Auto-generated on Saturday, July 12, 2025."""
-
-def main() -> None:
-    print("Hello from saturday_july_12th_2025.py")
-
-if __name__ == "__main__":
-    main()
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, ans = 0, 0
+        window_count = defaultdict(int)
+        
+        for r in range(len(s)):
+            window_count[s[r]] += 1
+            largest = max(window_count.items(), key=lambda x: x[1])[1]
+            # print(f"largest: {largest}, r: {r}, l: {l}")
+            if r - l + 1 - largest > k:
+                window_count[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+        
+        return ans
---
+++ 07-13-2025/sunday_july_13th_2025.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Sunday, July 13, 2025."""
+
+def main() -> None:
+    print("Hello from sunday_july_13th_2025.py")
+
+if __name__ == "__main__":
+    main()
