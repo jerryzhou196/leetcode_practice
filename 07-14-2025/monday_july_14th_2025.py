6e6b4909da6bf558ee5781677e9e021b021da8a1
Jerry Zhou <jerrzho@amazon.com>
2025-07-15 09:03:23 -0400

    🥳 Kicking off tuesday_july_15th_2025.py on 2025-07-15

--- 07-14-2025/monday_july_14th_2025.py
+++ 07-14-2025/monday_july_14th_2025.py
@@ -1,7 +1,24 @@
-"""Auto-generated on Monday, July 14, 2025."""
+class Solution:
+    def isInWindow(self, window, target_window):
+        for char in target_window.keys():
+            if window[char] < target_window[char]:
+                return False
+        return True
 
-def main() -> None:
-    print("Hello from monday_july_14th_2025.py")
+    def minWindow(self, s: str, t: str) -> str:
+        target_window = Counter(t)
+        window_count = Counter()
+        ans = [0, float('inf')]
 
-if __name__ == "__main__":
-    main()
+        left =  0
+        for r in range(len(s)):
+            window_count[s[r]] += 1
+            # print(r, window_count, target_window)
+            while left <= r and self.isInWindow(window_count, target_window):
+                # print(f"match: {left} - {r}", window_count, target_window)
+                if r - left < ans[1] - ans[0]:
+                    ans[0], ans[1] = left, r
+                window_count[s[left]] -= 1
+                left += 1
+        
+        return s[ans[0]:ans[1] + 1] if ans[1] < float('inf') else ""
---
+++ 07-15-2025/tuesday_july_15th_2025.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Tuesday, July 15, 2025."""
+
+def main() -> None:
+    print("Hello from tuesday_july_15th_2025.py")
+
+if __name__ == "__main__":
+    main()
