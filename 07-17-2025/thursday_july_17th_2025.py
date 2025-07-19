0279cc7b707a0362ecf4984d810b377c5b1c7f0a
Jerry Zhou <jerry.zhou@uwaterloo.ca>
2025-07-19 08:26:28 -0400

    🥳 Kicking off saturday_july_19th_2025.py on 2025-07-19

--- 07-17-2025/thursday_july_17th_2025.py
+++ 07-17-2025/thursday_july_17th_2025.py
@@ -1,7 +1,31 @@
 """Auto-generated on Thursday, July 17, 2025."""
 
-def main() -> None:
-    print("Hello from thursday_july_17th_2025.py")
+from typing import *
 
-if __name__ == "__main__":
-    main()
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = curr = ListNode(-1, None)
+        while list1 and list2: 
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2 
+                list2 = list2.next
+
+        while list1:
+            curr.next = list1
+            list1 = list1.next
+
+        while list2:
+            curr.next = list2
+            list2 = list2.next
+
+        return head
+
