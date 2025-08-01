54bd1905baa578b4a35b2210a8d341a828d596c9
Jerry Zhou <jerry.zhou@uwaterloo.ca>
2025-08-01 19:51:32 -0400

    💡 Drafted initial file for friday_august_1st_2025.py on 2025-08-01

--- 07-31-2025/thursday_july_31st_2025.py
+++ 07-31-2025/thursday_july_31st_2025.py
@@ -1,7 +1,37 @@
-"""Auto-generated on Thursday, July 31, 2025."""
+from typing import *
 
-def main() -> None:
-    print("Hello from thursday_july_31st_2025.py")
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-if __name__ == "__main__":
-    main()
+class Solution:
+    def revList(self, node, prev=None):
+        if node:
+            temp = node.next
+            node.next = prev
+            return node if not temp else self.revList(temp, node)
+
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow = fast = head
+        while fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # print(f"mid-point: {slow.val}")
+        
+        end = self.revList(slow, )
+        start = head 
+
+        while start: 
+            # print(f"start: {start.val}")
+            # print(f"end: {end.val}")
+            temp = start.next
+            start.next = end
+            temp1 = end.next 
+            end.next = temp
+            start = temp
+            end = temp1
---
+++ 08-01-2025/friday_august_1st_2025.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Friday, August 01, 2025."""
+
+def main() -> None:
+    print("Hello from friday_august_1st_2025.py")
+
+if __name__ == "__main__":
+    main()
