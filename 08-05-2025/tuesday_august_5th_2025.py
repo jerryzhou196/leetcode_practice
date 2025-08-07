c086cb4a63124b1ef6b6af375c11b126f8c1761e
Jerry Zhou <jerry.zhou@uwaterloo.ca>
2025-08-06 22:21:43 -0400

    💡 Drafted initial file for wednesday_august_6th_2025.py on 2025-08-06

--- 08-05-2025/tuesday_august_5th_2025.py
+++ 08-05-2025/tuesday_august_5th_2025.py
@@ -1,7 +1,35 @@
-"""Auto-generated on Tuesday, August 05, 2025."""
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        first = ListNode(-1, None)
+        ans = first
+        while l1 and l2:
+            ans.next = ListNode((l1.val + l2.val + carry) % 10, None)
+            ans = ans.next
+            print(f"ans.val {ans.val}")
+            
+            carry = (l1.val + l2.val + carry) // 10
+            l1 = l1.next
+            l2 = l2.next
+        
+        while l1: 
+            ans.next = ListNode((l1.val + carry) % 10, None)
+            ans = ans.next
+            print(f"ans.val {ans.val}")
 
-def main() -> None:
-    print("Hello from tuesday_august_5th_2025.py")
+            carry = (l1.val + carry) // 10
+            l1 = l1.next
 
-if __name__ == "__main__":
-    main()
+        while l2:
+            ans.next = ListNode((l2.val + carry) % 10, None)
+            ans = ans.next
+            print(f"ans.val {ans.val}")
+
+            carry = (l2.val + carry) // 10
+            l2 = l2.next
+
+        if carry:
+            ans.next = ListNode(carry, None)
+            print(f"ans.val {ans.val}")
+        
+        return first.next 
