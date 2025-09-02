bc6234beaa0bdd13dde0dec8133cad12db6d8a88
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-09-02 03:16:06 -0400

    🎉 Began work on tuesday_september_2nd_2025.py on 2025-09-02

--- 08-30-2025/saturday_august_30th_2025.py
+++ 08-30-2025/saturday_august_30th_2025.py
@@ -1,7 +1,47 @@
-"""Auto-generated on Saturday, August 30, 2025."""
+class ListNode:
+    def __init__(self, key, val, next=None, prev=None):
+        self.key = key
+        self.val = val
+        self.next = next
+        self.prev = prev
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.dict = {}
+        self.head = ListNode(None, None)
+        self.tail = ListNode(None, None) 
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.capacity = capacity
 
-def main() -> None:
-    print("Hello from saturday_august_30th_2025.py")
+    def add(self, key, val):
+        node = ListNode(key, val)
+        self.dict[key] = node
 
-if __name__ == "__main__":
-    main()
+        prev_last = self.tail.prev 
+        self.tail.prev = node 
+        node.next = self.tail
+
+        node.prev = prev_last
+        prev_last.next = node 
+
+    def remove(self, key):
+        node = self.dict[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        del self.dict[key]
+
+    def get(self, key: int) -> int:
+        val = -1
+        if key in self.dict:
+            val = self.dict[key].val
+            self.remove(key)
+            self.add(key, val)
+        return val
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            self.remove(key)
+        elif len(self.dict) == self.capacity:
+            self.remove(self.head.next.key)
+        self.add(key, value)
---
+++ 09-02-2025/tuesday_september_2nd_2025.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Tuesday, September 02, 2025."""
+
+def main() -> None:
+    print("Hello from tuesday_september_2nd_2025.py")
+
+if __name__ == "__main__":
+    main()
