82cb88e67d1f393d3c1862e4f66312691c683549
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-09-12 21:30:26 -0400

    🎯 Starting point for friday_september_12th_2025.py on 2025-09-12

--- 09-02-2025/tuesday_september_2nd_2025.py
+++ 09-02-2025/tuesday_september_2nd_2025.py
@@ -1,7 +1,36 @@
-"""Auto-generated on Tuesday, September 02, 2025."""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-def main() -> None:
-    print("Hello from tuesday_september_2nd_2025.py")
+from typing import *
 
-if __name__ == "__main__":
-    main()
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        lists_dict = {}
+        for index, l in enumerate(lists):
+            if l:
+                lists_dict[index] = l 
+        
+        # print(lists_dict)
+        
+        ans = curr = ListNode() 
+        while lists_dict:
+            curr_smallest = next(iter(lists_dict)) 
+            for index in lists_dict.keys():
+                if lists_dict[curr_smallest].val > lists_dict[index].val:
+                    curr_smallest = index
+
+            # print(lists_dict[curr_smallest].val)
+
+            ans.next = lists_dict[curr_smallest]
+            ans = ans.next
+
+            if lists_dict[curr_smallest].next:
+                lists_dict[curr_smallest] = lists_dict[curr_smallest].next 
+            else:
+                del lists_dict[curr_smallest]
+        
+        return curr.next
+        
