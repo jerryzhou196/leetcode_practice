c9f965732b142e4e58a1a1d9cd97f9f0afb6a8ba
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-05-27 08:37:26 -0600

    🌟 New LeetCode problem: wednesday_may_27th_2026.py on 2026-05-27

--- 05-26-2026/tuesday_may_26th_2026.py
+++ 05-26-2026/tuesday_may_26th_2026.py
@@ -1,7 +1,22 @@
-"""Auto-generated on Tuesday, May 26, 2026."""
+class Solution:
+    def jump(self, nums: List[int]) -> int:
 
-def main() -> None:
-    print("Hello from tuesday_may_26th_2026.py")
+        n = len(nums)
+        if len(nums) == 1: return 0
+        
+        jumps = 0
+        i = 0
+        maximum_distance = nums[0] 
+        can_jump = 1 
+        while True: 
+            start = i + 1
+            # print(start, maximum_distance + 1)
+            for j in range(start, maximum_distance + 1): 
+                if j >= n - 1: return jumps + 1
+                can_jump = max(j + nums[j], can_jump)
+                i += 1
 
-if __name__ == "__main__":
-    main()
+            jumps += 1
+            maximum_distance = can_jump
+
+# [2, 2, 4, 1000, 0, 0, 0, 1]
---
+++ 05-27-2026/wednesday_may_27th_2026.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Wednesday, May 27, 2026."""
+
+def main() -> None:
+    print("Hello from wednesday_may_27th_2026.py")
+
+if __name__ == "__main__":
+    main()
