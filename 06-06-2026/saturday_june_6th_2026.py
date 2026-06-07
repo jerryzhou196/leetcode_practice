ac1296010ef1463f7d4e27cb1801c371c707cdb3
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-06-07 07:57:28 -0600

    🌟 New LeetCode problem: sunday_june_7th_2026.py on 2026-06-07

--- 06-06-2026/saturday_june_6th_2026.py
+++ 06-06-2026/saturday_june_6th_2026.py
@@ -1,7 +1,33 @@
-"""Auto-generated on Saturday, June 06, 2026."""
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n, m = len(board), len(board[0])
+        def mapEdge(y, x): 
+            if y < 0 or y >= n or x < 0 or x >= m or board[y][x] == 'K' or board[y][x] == 'X': 
+                return 
+            
+            board[y][x] = 'K'
 
-def main() -> None:
-    print("Hello from saturday_june_6th_2026.py")
+            for i, j in [[0,1], [1,0], [-1, 0], [0, -1]]:
+                mapEdge(y + i, x + j)
+        
+        for j in range(m):
+            mapEdge(0, j)
+            mapEdge(n - 1, j)
 
-if __name__ == "__main__":
-    main()
+        for i in range(n):
+            mapEdge(i, 0)
+            mapEdge(i, m - 1)
+        
+        # print(board)
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == 'K':
+                    board[i][j] = 'O'
+                else:
+                    board[i][j] = 'X'
+        
+        return
---
+++ 06-07-2026/sunday_june_7th_2026.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Sunday, June 07, 2026."""
+
+def main() -> None:
+    print("Hello from sunday_june_7th_2026.py")
+
+if __name__ == "__main__":
+    main()
