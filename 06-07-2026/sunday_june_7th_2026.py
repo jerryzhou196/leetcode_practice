8690ed35c3a7c3874171a52b51dbf3c69b27ff89
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-06-08 07:25:04 -0600

    🥳 Kicking off monday_june_8th_2026.py on 2026-06-08

--- 06-07-2026/sunday_june_7th_2026.py
+++ 06-07-2026/sunday_june_7th_2026.py
@@ -1,7 +1,24 @@
-"""Auto-generated on Sunday, June 07, 2026."""
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = {}
+        # edges = defaultdict(list)
+        for i, (y, x) in enumerate(edges, 1):
+            parent[i] = i 
+            # edges[y] = x
 
-def main() -> None:
-    print("Hello from sunday_june_7th_2026.py")
+        def find(a):
+            if a != parent[a]:
+                parent[a] = find(parent[a])
+            return parent[a]
 
-if __name__ == "__main__":
-    main()
+        def union(a, b):
+            root = find(a)
+            root1 = find(b)
+            parent[root] = root1
+
+        for a, b in edges:         
+            if find(a) == find(b):
+                return [a, b]
+            else:
+                union(a, b)
+
