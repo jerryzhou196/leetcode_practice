24495f54abb681e2f0e81479a3c72f0b0eede99c
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-06-11 11:28:43 -0600

    🥳 Kicking off thursday_june_11th_2026.py on 2026-06-11

--- 06-10-2026/wednesday_june_10th_2026.py
+++ 06-10-2026/wednesday_june_10th_2026.py
@@ -1,7 +1,27 @@
-"""Auto-generated on Wednesday, June 10, 2026."""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False for _ in range(n)] for _ in range(n)]
+        ans_string = ""
 
-def main() -> None:
-    print("Hello from wednesday_june_10th_2026.py")
+        for i in range(n):
+            dp[i][i] = True
+            if 1 > len(ans_string):
+                ans_string = s[i:i + 1]
+            if i < n - 1 and s[i] == s[i + 1]: 
+                dp[i][i + 1] = True
+                if 2 > len(ans_string): 
+                    ans_string = s[i:i + 2]
+        
+        for i in range(2, n):
+            for j in range(n - i):
+                # print(j, j + i)
+                if dp[j + 1][j + i - 1] and s[j] == s[j + i]:
+                    if i + 1 > len(ans_string): 
+                        ans_string = s[j:j + i + 1]
+                    dp[j][j + i] = True
+        
+        return ans_string
 
-if __name__ == "__main__":
-    main()
+            
+        
