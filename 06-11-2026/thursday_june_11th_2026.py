7ef249d0019a79e5d9be6590289f36c70afa6b8b
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-06-12 08:15:52 -0600

    💡 Drafted initial file for friday_june_12th_2026.py on 2026-06-12

--- 06-11-2026/thursday_june_11th_2026.py
+++ 06-11-2026/thursday_june_11th_2026.py
@@ -1,7 +1,33 @@
-"""Auto-generated on Thursday, June 11, 2026."""
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        def isValid(c):
+            if c[0] == '0': return False
+            return 1 <= int(c) <= 26
 
-def main() -> None:
-    print("Hello from thursday_june_11th_2026.py")
+        n = len(s)
+        dp = [0 for _ in range(n)]
+        dp[0] = 1 if isValid(s[0]) else 0
 
-if __name__ == "__main__":
-    main()
+        if n <= 1: return dp[0]
+        dp[1] = 0
+        if isValid(s[1]):
+            dp[1] = dp[0]
+        if isValid(s[0] + s[1]):
+            dp[1] += 1
+
+        for i in range(2, n): 
+            dp[i] = 0
+            if isValid(s[i]):
+                dp[i] += dp[i - 1]
+            if isValid(s[i - 1] + s[i]):
+                dp[i] += dp[i - 2]
+        
+        return dp[n - 1]
+            
+            
+            
+
+
+        # dp[i] = dp[i - 1] + 1 if isValid(s[i] + s[i - 1]) and s[i] + s[i - 1] <= 26 else dp[i - 1]
+        # return 5
+        
---
+++ 06-12-2026/friday_june_12th_2026.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Friday, June 12, 2026."""
+
+def main() -> None:
+    print("Hello from friday_june_12th_2026.py")
+
+if __name__ == "__main__":
+    main()
