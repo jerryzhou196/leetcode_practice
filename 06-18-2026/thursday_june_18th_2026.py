8a84e6bff68d712fd0851a53d0813365e778b56c
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-06-19 10:23:12 -0600

    💡 Drafted initial file for friday_june_19th_2026.py on 2026-06-19

--- 06-18-2026/thursday_june_18th_2026.py
+++ 06-18-2026/thursday_june_18th_2026.py
@@ -1,7 +1,20 @@
-"""Auto-generated on Thursday, June 18, 2026."""
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        n, m = len(matrix), len(matrix[0])
 
-def main() -> None:
-    print("Hello from thursday_june_18th_2026.py")
-
-if __name__ == "__main__":
-    main()
+        @lru_cache(maxsize=100000)
+        def dfs(i, j):
+            # print(i, j, matrix[i][j])
+            ans = 1 
+            for y, x in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+                yy, xx = i + y, j + x
+                if yy >= 0 and yy < n and xx >= 0 and xx < m and matrix[yy][xx] > matrix[i][j]:
+                    ans = max(ans, 1 + dfs(yy, xx))
+            return ans
+    
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                ans = max(ans, dfs(i, j))
+        
+        return ans 
---
+++ 06-19-2026/friday_june_19th_2026.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Friday, June 19, 2026."""
+
+def main() -> None:
+    print("Hello from friday_june_19th_2026.py")
+
+if __name__ == "__main__":
+    main()
