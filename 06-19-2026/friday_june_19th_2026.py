c07f96896bc47ef839586147ec38a1d24bcab243
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-06-20 08:16:21 -0600

    🌟 New LeetCode problem: saturday_june_20th_2026.py on 2026-06-20

--- 06-19-2026/friday_june_19th_2026.py
+++ 06-19-2026/friday_june_19th_2026.py
@@ -1,7 +1,27 @@
-"""Auto-generated on Friday, June 19, 2026."""
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n,m = len(s), len(t)
 
-def main() -> None:
-    print("Hello from friday_june_19th_2026.py")
 
-if __name__ == "__main__":
-    main()
+        dp = [0] * (m + 1)
+        # dp[0] = 1 
+
+        # dp[i][j]: subsequences in s[:j] that match to t[:i]
+        # dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+        # print(dp)
+
+        for i in range(1, n + 1):
+            next_dp = [0] * (m + 1)
+            dp[0] = 1
+            for j in range(1, m + 1):
+                next_dp[j] = dp[j]
+                if s[i - 1] == t[j - 1]:
+                    next_dp[j] += dp[j - 1]
+            dp = next_dp
+        
+        return dp[m]
+            
+
+
+
+
