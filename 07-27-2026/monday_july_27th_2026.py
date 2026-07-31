4bc838c80df53558dd2330b3bfe83753cbff70dd
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-07-31 12:17:22 -0600

    🎉 Began work on friday_july_31st_2026.py on 2026-07-31

--- 07-27-2026/monday_july_27th_2026.py
+++ 07-27-2026/monday_july_27th_2026.py
@@ -1,7 +1,13 @@
-"""Auto-generated on Monday, July 27, 2026."""
-
-def main() -> None:
-    print("Hello from monday_july_27th_2026.py")
-
-if __name__ == "__main__":
-    main()
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        max_diff = 0
+        largest, smallest = arrays[0][0], arrays[0][-1] 
+        for array in arrays:
+            max_diff = max(abs(array[-1] - smallest), max_diff)
+            max_diff = max(abs(array[0] - largest), max_diff)
+            
+            largest = max(largest, array[-1])
+            smallest = min(smallest, array[0])
+    
+        return max_diff
+            
---
+++ 07-31-2026/friday_july_31st_2026.py
@@ -0,0 +1,7 @@
+"""Auto-generated on Friday, July 31, 2026."""
+
+def main() -> None:
+    print("Hello from friday_july_31st_2026.py")
+
+if __name__ == "__main__":
+    main()
